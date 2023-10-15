5dd0cd8b9a0d681c0fc984a89467556afec977f8
shubham9324 <sharmashiv972@gmail.com>
2023-10-15 23:16:25 +0530

    This is first commit of Namaste React

---
+++ app.js
@@ -0,0 +1,23 @@
+const parent = React.createElement('div',
+{
+    id:'Parent'
+},
+[
+    React.createElement('div',
+{id:'Child1'},
+[
+    React.createElement('h1',{style:{background:'red',color:'white'}},'I am an H1 Tag'),
+    React.createElement('h2',{},'I am an H2 Tag')
+]
+),
+React.createElement('div',
+{id:'Child2'},
+[
+    React.createElement('h1',{},'I am an H1 Tag'),
+    React.createElement('h2',{},'I am an H2 Tag')
+]
+)
+]
+);
+        const root = ReactDOM.createRoot(document.getElementById('root'));
+        root.render(parent);
